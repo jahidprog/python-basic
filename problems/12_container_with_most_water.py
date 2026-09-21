a65d05859cc9bdc0a89151c5d068a5e315434afb
Jahid-prog <jahid.prog@gmail.com>
2026-09-21 14:12:42 +0600

    problems: containter with most water

--- problems/12_container_with_most_water.py
+++ problems/12_container_with_most_water.py
@@ -3,11 +3,25 @@ max_water = 0
 n = len(height)
 
 # Brute force O(n^2)
-for i in range(n):
-    for j in range(i+1, n):
-        w = j - i
-        ht = min(height[i], height[j])
+# for i in range(n):
+#     for j in range(i+1, n):
+#         w = j - i
+#         ht = min(height[i], height[j])
 
-        area = w * ht
-        max_water = max(max_water, area)
+#         area = w * ht
+#         max_water = max(max_water, area)
+# print(max_water)
+left = 0
+right = n - 1
+
+while(left < right):
+    w = right - left
+    ht = min(height[left], height[right])
+    a = w * ht
+    max_water = max(a, max_water)
+    if height[left] < height[right]:
+        left += 1
+    else:
+        right -= 1
+
 print(max_water)
