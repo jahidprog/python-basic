a3e75192596cb73817fae661e21f97df40392cbd
Jahid-prog <jahid.prog@gmail.com>
2026-09-22 16:12:48 +0600

    problems: 0s, 1s, 2s with Dutch National Flag Algo

--- problems/13_sort_colors.py
+++ problems/13_sort_colors.py
@@ -1,23 +1,17 @@
-nums = [2, 0, 1, 0, 2, 0, 1, 1, 0, 2, 2, 2,0,2,1,1,0]
+nums = [2, 0, 1]
 
-cn0, cn1, cn2 = 0, 0, 0
-for i in range(len(nums)):
-    if nums[i] == 0:
-        cn0 += 1
-    elif nums[i] == 1:
-        cn1 += 1
+n = len(nums)
+m, l, h = 0, 0, n-1
+
+while m <= h:
+    if nums[m] == 0:
+        nums[l], nums[m] = nums[m], nums[l]
+        m += 1
+        l += 1
+    elif nums[m] == 1:
+        m += 1
     else:
-        cn2 += 1
-
-idx = 0
-for i in range(cn0):
-    nums[idx] = 0
-    idx += 1
-for i in range(cn1):
-    nums[idx] = 1
-    idx += 1
-for i in range(cn2):
-    nums[idx] = 2
-    idx += 1
+        nums[h], nums[m] = nums[m], nums[h]
+        h -= 1
 
 print(nums)
