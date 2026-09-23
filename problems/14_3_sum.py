73ee5f6bb1d09f19b9f69b11421f4a0a6da12eb2
Jahid-prog <jahid.prog@gmail.com>
2026-09-23 19:35:24 +0600

    problems: 3 sum brute force approch

---
+++ problems/14_3_sum.py
@@ -0,0 +1,37 @@
+# 3Sum - Brute Force O(n^3)
+
+
+def three_sum(nums):
+    n = len(nums)
+    ans = []
+
+    s = set()
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+
+                if nums[i] + nums[j] + nums[k] == 0:
+                    trip = [nums[i], nums[j], nums[k]]
+
+                    trip.sort()
+                    trip = tuple(trip)
+
+                    if trip not in s:
+                        s.add(trip)
+                        ans.append(list(trip))
+
+    return ans
+
+
+def main():
+    nums = [-1, 0, 1, 2, -1, -4]
+
+    result = three_sum(nums)
+
+    print("Input:", nums)
+    print("Three Sum:", result)
+
+
+if __name__ == "__main__":
+    main()
