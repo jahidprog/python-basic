8d5a6081307ed999add0e36caf04c7f6c7fceefa
Jahid-prog <jahid.prog@gmail.com>
2026-09-24 08:15:46 +0600

    prblems: 3 sum o^2 solution

--- problems/14_3_sum.py
+++ problems/14_3_sum.py
@@ -1,26 +1,46 @@
 # 3Sum - Brute Force O(n^3)
+# def three_sum(nums):
+#     n = len(nums)
+#     ans = []
+#     s = set()
+
+#     for i in range(n):
+#         for j in range(i+1, n):
+#             for k in range(j+1,n):
+#                 if nums[i] + nums[j] + nums[k] == 0:
+#                     trip = [nums[i], nums[j], nums[k]]
+#                     trip.sort()
+#                     trip = tuple(trip)
+
+#                     if trip not in s:
+#                         s.add(trip)
+#                         ans.append(list(trip))
+#     return ans
+
+
+# 3Sum - O(n^2)
 def three_sum(nums):
     n = len(nums)
     ans = []
-
-    s = set()
+    unique_s = set()
 
     for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
+        target = -nums[i]
+        s = set()
 
-                if nums[i] + nums[j] + nums[k] == 0:
-                    trip = [nums[i], nums[j], nums[k]]
+        for j in range(i+1, n):
+            third = target - nums[j]
 
-                    trip.sort()
-                    trip = tuple(trip)
+            if third in s:
+                trip = [nums[i], nums[j], third]
+                trip.sort()
+                trip = tuple(trip)
 
-                    if trip not in s:
-                        s.add(trip)
-                        ans.append(list(trip))
-
+                if trip not in unique_s:
+                    unique_s.add(trip)
+                    ans.append(list(trip))
+            s.add(nums[j])
     return ans
-
 
 def main():
     nums = [-1, 0, 1, 2, -1, -4]
@@ -33,4 +53,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-# O^2 solution
